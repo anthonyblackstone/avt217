62365031d6d93fe90b164a4db5af371863143008
anthonyblackstone <54603032+anthonyblackstone@users.noreply.github.com>
2019-11-12 19:12:12 -0500

    11/12

---
+++ e17/js/main.js
@@ -0,0 +1,15 @@
+$( document ).ready(function() {
+  console.log('THE DOCUMENT IS READY! DO SOMETHING AWESOME!')
+
+  $('.loz-oot').click(function() {
+    $('.number-1').toggleClass('align-left', 'left');
+  })
+
+  $('.fallout3').click(function() {
+    $('.number-2').slideToggle();
+  })
+
+  $('.ff7').click(function() {
+    $('.number-3').css('background-color', 'green');
+  })
+});
